fbd48a050946b2b5fba68548f0df29b07790f259
Muaaz Cassim <muaazc@gmail.com>
2018-09-17 01:33:26 +0200

    Add Supplier View and Controller with Validation Complete

--- CompuData/Controllers/AddSupplierController.cs
+++ CompuData/Controllers/AddSupplierController.cs
@@ -11,7 +11,61 @@ namespace CompuData.Controllers
         // GET: AddSupplier
         public ActionResult Index()
         {
-            return View();
+            var db = new CodeFirst.CodeFirst();
+            var supplier = new Models.Supplier();
+            return View(supplier);
+        }
+
+        [HttpPost]
+        public ActionResult Create([Bind(Prefix = "")]Models.Supplier model)
+        {
+            var db = new CodeFirst.CodeFirst();
+            if (ModelState.IsValid)
+            {
+                if (db.Suppliers.Count() > 0)
+                {
+                    var item = db.Suppliers.OrderByDescending(a => a.SupplierID).FirstOrDefault();
+
+                    db.Suppliers.Add(new CodeFirst.Supplier
+                    {
+                        SupplierID = item.SupplierID + 1,
+                        Name = model.Name,
+                        VATNumber = model.VATNumber,
+                        EmailAddress = model.EmailAddress,
+                        ContactNumber = model.ContactNumber,
+                        Bank = model.Bank,
+                        AccountNumber = model.AccountNumber,
+                        BranchCode = model.BranchCode,
+                        POAddress = model.POAddress,
+                        POCity = model.POCity,
+                        POAreaCode = model.POAreaCode
+                });
+                }
+                else
+                {
+                    db.Suppliers.Add(new CodeFirst.Supplier
+                    {
+                        SupplierID = 1,
+                        Name = model.Name,
+                        VATNumber = model.VATNumber,
+                        EmailAddress = model.EmailAddress,
+                        ContactNumber = model.ContactNumber,
+                        Bank = model.Bank,
+                        AccountNumber = model.AccountNumber,
+                        BranchCode = model.BranchCode,
+                        POAddress = model.POAddress,
+                        POCity = model.POCity,
+                        POAreaCode = model.POAreaCode
+                    });
+                }
+
+                db.SaveChanges();
+                model.JavaScriptToRun = "mySuccess()";
+                TempData["model"] = model;
+                return RedirectToAction("Index", "Suppliers");
+            }
+
+            return View("Index", model);
         }
     }
 }
