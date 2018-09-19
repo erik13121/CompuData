a225dd6a8a8b6aaba9f5e217c43df4a698d5bc0e
Muaaz Cassim <muaazc@gmail.com>
2018-09-19 02:23:04 +0200

    Modify And View Equipment Type

---
+++ CompuData/Controllers/EquipmentTypeDetailsController.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace CompuData.Controllers
+{
+    public class EquipmentTypeDetailsController : Controller
+    {
+        // GET: EquipmentTypeDetails
+        public ActionResult Index(string typeID)
+        {
+            Models.EquipmentType myModel = new Models.EquipmentType();
+            CodeFirst.CodeFirst db = new CodeFirst.CodeFirst();
+            if (typeID != null)
+            {
+                var intTypeID = Int32.Parse(typeID);
+                var myEquipType = db.Equipment_Type.Where(i => i.TypeID == intTypeID).FirstOrDefault();
+
+                myModel.TypeID = myEquipType.TypeID;
+                myModel.TypeName = myEquipType.TypeName;
+                myModel.TypeDescription = myEquipType.TypeDescription;
+                myModel.Removable = myEquipType.Removable;
+
+            }
+
+            return View(myModel);
+        }
+
+        [HttpPost]
+        public ActionResult RedirectToEquipTypeDetails(string typeID)
+        {
+            var redirectUrl = new UrlHelper(Request.RequestContext).Action("Index", "EquipmentTypeDetails", new { typeID = typeID });
+            return Json(new { Url = redirectUrl });
+        }
+    }
+}
---
+++ CompuData/Controllers/ModifyEquipmentTypeController.cs
@@ -0,0 +1,86 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace CompuData.Controllers
+{
+    public class ModifyEquipmentTypeController : Controller
+    {
+        // GET: ModifyEquipmentType
+        public ActionResult Index(string TypeID)
+        {
+            CodeFirst.CodeFirst db = new CodeFirst.CodeFirst();
+            Models.EquipmentType myModel = new Models.EquipmentType();
+            if (TypeID != null)
+            {
+                var intEquipID = Int32.Parse(TypeID);
+                var myEquipType = db.Equipment_Type.Where(i => i.TypeID == intEquipID).FirstOrDefault();
+
+                myModel.TypeID = myEquipType.TypeID;
+                myModel.TypeName = myEquipType.TypeName;
+                myModel.TypeDescription = myEquipType.TypeDescription;
+                myModel.Removable = myEquipType.Removable;
+
+                return View(myModel);
+            }
+
+            return View(myModel);
+        }
+
+        [HttpPost]
+        public ActionResult Index(Models.EquipmentType model)
+        {
+            CodeFirst.CodeFirst db = new CodeFirst.CodeFirst();
+
+            if (ModelState.IsValid)
+            {
+                Models.EquipmentType myModel = new Models.EquipmentType();
+
+                var myEquipType = db.Equipment_Type.Where(i => i.TypeID == model.TypeID).FirstOrDefault();
+
+                myModel.TypeID = myEquipType.TypeID;
+                myModel.TypeName = myEquipType.TypeName;
+                myModel.TypeDescription = myEquipType.TypeDescription;
+                myModel.Removable = myEquipType.Removable;
+
+
+                return View(myModel);
+            }
+
+            return View(model);
+        }
+
+        [HttpPost]
+        public ActionResult RedirectToModifyEquipTypeDetails(string typeID)
+        {
+            var redirectUrl = new UrlHelper(Request.RequestContext).Action("Index", "ModifyEquipmentType", new { typeID = typeID });
+            return Json(new { Url = redirectUrl });
+        }
+
+        [HttpPost]
+        public ActionResult Modify([Bind(Prefix = "")]Models.EquipmentType model)
+        {
+            var db = new CodeFirst.CodeFirst();
+            if (ModelState.IsValid)
+            {
+                var EquipType = db.Equipment_Type.Where(v => v.TypeID == model.TypeID).SingleOrDefault();
+
+                if (EquipType != null)
+                {
+                    EquipType.TypeID = model.TypeID;
+                    EquipType.TypeName = model.TypeName;
+                    EquipType.TypeDescription = model.TypeDescription;
+                    EquipType.Removable = model.Removable;
+                    db.SaveChanges();
+                }
+
+                TempData["js"] = "myUpdateSuccess()";
+                return RedirectToAction("Index", "EquipmentType");
+            }
+
+            return View("Index", model);
+        }
+    }
+}
