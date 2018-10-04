fef71ad627f4425e0085bc5e6f92116bd5afdf22
Muaaz Cassim <muaazc@gmail.com>
2018-10-04 15:46:50 +0200

    ReportEquipmentBookings

--- CompuData/Controllers/ReportEquipmentBookingsController.cs
+++ CompuData/Controllers/ReportEquipmentBookingsController.cs
@@ -1,5 +1,7 @@
-﻿using System;
+﻿using CrystalDecisions.CrystalReports.Engine;
+using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
@@ -12,6 +14,26 @@ namespace CompuData.Controllers
         public ActionResult Index()
         {
             return View();
+        }
+        public ActionResult EquipmentRegister(DateTime fromDate, DateTime toDate)
+        {
+            ReportDocument rd = new ReportDocument();
+            rd.Load(Path.Combine(Server.MapPath("~/Report/EquipmentBookingsReport.rpt")));
+            rd.SetParameterValue("@startdate", fromDate);
+            rd.SetParameterValue("@enddate", toDate);
+            Response.Buffer = false;
+            Response.ClearContent();
+            Response.ClearHeaders();
+            try
+            {
+                Stream stream = rd.ExportToStream(CrystalDecisions.Shared.ExportFormatType.PortableDocFormat);
+                stream.Seek(0, SeekOrigin.Begin);
+                return File(stream, "application/pdf", "EquipmentBookingsReport.pdf");
+            }
+            catch
+            {
+                throw;
+            }
         }
     }
 }
