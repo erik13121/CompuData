8b51efbd864180461d54b70ce5359b425debfafa
Muaaz Cassim <muaazc@gmail.com>
2018-10-04 15:41:20 +0200

    Added New Controller

---
+++ CompuData/Controllers/ReportEquipmentBookingsController.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace CompuData.Controllers
+{
+    public class ReportEquipmentBookingsController : Controller
+    {
+        // GET: ReportEquipmentBookings
+        public ActionResult Index()
+        {
+            return View();
+        }
+    }
+}
