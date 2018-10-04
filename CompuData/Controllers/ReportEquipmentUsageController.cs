cfa8d3a52bbd102f89dfaafdb6d70647aecc3466
Muaaz Cassim <muaazc@gmail.com>
2018-10-04 15:25:41 +0200

    Reports Changes

---
+++ CompuData/Controllers/ReportEquipmentUsageController.cs
@@ -0,0 +1,40 @@
+﻿using CrystalDecisions.CrystalReports.Engine;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace CompuData.Controllers
+{
+    public class ReportEquipmentUsageController : Controller
+    {
+        // GET: ReportEquipmentUsage
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        public ActionResult EquipmentUsage(DateTime fromDate, DateTime toDate)
+        {
+            ReportDocument rd = new ReportDocument();
+            rd.Load(Path.Combine(Server.MapPath("~/Report/EquipmentUsage.rpt")));
+            rd.SetParameterValue("@startdate", fromDate);
+            rd.SetParameterValue("@enddate", toDate);
+            Response.Buffer = false;
+            Response.ClearContent();
+            Response.ClearHeaders();
+            try
+            {
+                Stream stream = rd.ExportToStream(CrystalDecisions.Shared.ExportFormatType.PortableDocFormat);
+                stream.Seek(0, SeekOrigin.Begin);
+                return File(stream, "application/pdf", "EquipmentUsageReport.pdf");
+            }
+            catch
+            {
+                throw;
+            }
+        }
+    }
+}
